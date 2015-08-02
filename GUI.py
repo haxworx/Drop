fb5b4881ade92d64e260169f1b4c719840ad1448
Al Poole <netstar@gmail.com>
2015-08-02 15:17:40 +0100

    BOGUS!!

--- GUI.py
+++ GUI.py
@@ -7,33 +7,35 @@ import subprocess
 from tkinter import *
 from queue import Queue
 import time
+import os
+import signal 
 
+		
 class Application(Frame):
 	def __init__(self, master):
 		self.myParent = master;
+		self.pro = None
 		super(Application, self).__init__(master)
 		self.active = False;
 		self.grid()
 		self.create_widgets()
-	def test(self, string):
-		print("pressed the bum button\n")
-		p = subprocess.Popen(string, stdout=subprocess.PIPE)
-		out, err = p.communicate()
-		print(out.decode())
-	#	self.textbox.insert(INSERT, out.decode()) this is breaking!!!!
+
+	def work(self, string):
+		self.pro = subprocess.call(string, shell=True)
 
 	def worker(self, string):
 		self.active = False;
-		t = Thread(target=self.test, args=(string,))
+		print("pressed the bum button\n")
+		t = Thread(target=self.work, args=(string,))
 		t.start()
+		
 	
 	def start(self):
 		exe = self.entry.get()
 		self.worker(exe)
 		
 	def stop(self):
-		self.t.kill()
-		self.active = False;
+		self.pro.terminate()
 
 	def create_widgets(self):
 		self.frame = Frame(self)
@@ -46,8 +48,8 @@ class Application(Frame):
 		self.stop_button = Button(self.frame, text = "Stop", command = self.stop)
 		self.stop_button.grid(row=0, column =3)
 		
-		self.textbox = Text(self.frame )
-		self.textbox.grid(row=1, column = 0, columnspan=4 ,sticky=W+E+N+S)
+		#self.textbox = Text(self.frame )
+		#self.textbox.grid(row=1, column = 0, columnspan=4 ,sticky=W+E+N+S)
 def main():
 	root = Tk()
 	root.title("Bogotron Controller")
